cedc02f69dd5634d8f0af8278a147854e2f3b628
Iqbal Hassan <iqbal@hasprime.com>
2024-11-11 16:09:45 +0800

    MDEV-13139: CREATE PACKAGE: package-wide declarations

--- sql/item_cmpfunc.h
+++ sql/item_cmpfunc.h
@@ -3736,8 +3736,11 @@ public:
 class Item_func_cursor_bool_attr: public Item_bool_func, public Cursor_ref
 {
 public:
-  Item_func_cursor_bool_attr(THD *thd, const LEX_CSTRING *name, uint offset)
-   :Item_bool_func(thd), Cursor_ref(name, offset)
+  Item_func_cursor_bool_attr(THD *thd,
+                             const LEX_CSTRING *name,
+                             uint offset,
+                             const Sp_rcontext_handler *rh)
+   :Item_bool_func(thd), Cursor_ref(name, offset, rh)
   { }
   bool check_vcol_func_processor(void *arg) override
   {
@@ -3753,8 +3756,11 @@ public:
 class Item_func_cursor_isopen: public Item_func_cursor_bool_attr
 {
 public:
-  Item_func_cursor_isopen(THD *thd, const LEX_CSTRING *name, uint offset)
-   :Item_func_cursor_bool_attr(thd, name, offset) { }
+  Item_func_cursor_isopen(THD *thd,
+                          const LEX_CSTRING *name,
+                          uint offset,
+                          const Sp_rcontext_handler *rh)
+   :Item_func_cursor_bool_attr(thd, name, offset, rh) { }
   LEX_CSTRING func_name_cstring() const override
   {
     static LEX_CSTRING name= {STRING_WITH_LEN("%ISOPEN") };
@@ -3769,8 +3775,11 @@ public:
 class Item_func_cursor_found: public Item_func_cursor_bool_attr
 {
 public:
-  Item_func_cursor_found(THD *thd, const LEX_CSTRING *name, uint offset)
-   :Item_func_cursor_bool_attr(thd, name, offset)
+  Item_func_cursor_found(THD *thd,
+                         const LEX_CSTRING *name,
+                         uint offset,
+                         const Sp_rcontext_handler *rh)
+   :Item_func_cursor_bool_attr(thd, name, offset, rh)
   {
     set_maybe_null();
   }
@@ -3788,8 +3797,11 @@ public:
 class Item_func_cursor_notfound: public Item_func_cursor_bool_attr
 {
 public:
-  Item_func_cursor_notfound(THD *thd, const LEX_CSTRING *name, uint offset)
-   :Item_func_cursor_bool_attr(thd, name, offset)
+  Item_func_cursor_notfound(THD *thd,
+                            const LEX_CSTRING *name,
+                            uint offset,
+                            const Sp_rcontext_handler *rh)
+   :Item_func_cursor_bool_attr(thd, name, offset, rh)
   {
     set_maybe_null();
   }
--- sql/sp_pcontext.h
+++ sql/sp_pcontext.h
@@ -653,6 +653,8 @@ public:
   sp_condition_value *find_condition(const LEX_CSTRING *name,
                                      bool current_scope_only) const;
 
+  sp_condition_value *find_predefined_condition(const LEX_CSTRING *name) const;
+
   sp_condition_value *
   find_declared_or_predefined_condition(THD *thd, const LEX_CSTRING *name) const;
 
@@ -808,8 +810,6 @@ private:
   /* Prevent use of these */
   sp_pcontext(const sp_pcontext &);
   void operator=(sp_pcontext &);
-
-  sp_condition_value *find_predefined_condition(const LEX_CSTRING *name) const;
 
 private:
   /// m_max_var_index -- number of variables (including all types of arguments)
